be47d0c4b6a0425c49fa8b7d608ca4ab0d1dd500
Hiren Joshi <hirenj@gmail.com>
2014-05-11 21:19:23 +0200

    Add in better frame subsetting

--- R/SiteReader.R
+++ R/SiteReader.R
@@ -254,7 +254,7 @@ downloadDataset <- function(set,config,accs=c(),etagcheck=TRUE) {
 
       # We should get a data frame out from the jsonParser - attach the uniprot id as
       # another column into the data frame
-
+      
       frm$uniprot <- rep(uprot,dim(frm)[1])
       return(frm)
     },.progress="text"))
@@ -263,7 +263,7 @@ downloadDataset <- function(set,config,accs=c(),etagcheck=TRUE) {
     # ends up as the first column for consistency
 
     wanted_cols <- names(frame)
-    frame <- frame[,c('uniprot',wanted_cols[!wanted_cols == 'uniprot'])]
+    frame <- subset(frame,select=c('uniprot',wanted_cols[!wanted_cols == 'uniprot']))
     setnames(frame, c('uniprot', data$defaults$rKeys, rep('NA',dim(frame)[2] - (length(data$defaults$rKeys)+1))))
   } else {
     # Assume that we're just pulling out sites from the data sets if we're not given a particular
@@ -326,11 +326,12 @@ testParseJson <- function(filename) {
     frm$uniprot <- rep(uprot,dim(frm)[1])
     return(frm)
   },.progress="text"))
+
   # We need to re-arrange the columns here so that the uniprot column
   # ends up as the first column for consistency
 
   wanted_cols <- names(frame)
-  frame <- frame[,c('uniprot',wanted_cols[!wanted_cols == 'uniprot'])]
+  frame <- subset(frame,select=c('uniprot',wanted_cols[!wanted_cols == 'uniprot']))
   setnames(frame, c('uniprot', origData$defaults$rKeys, rep('NA',dim(frame)[2] - (length(origData$defaults$rKeys)+1))))
 
   if (!is.null(origData$defaults$rNames)) {
