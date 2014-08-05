eeb4e2d88756efbd325ee360f29ddd168db15c07
Hiren Joshi <hirenj@gmail.com>
2014-08-05 14:19:33 +0200

    Add in docs for calculateSequenceWindow

--- R/calculateSequenceWindow.R
+++ R/calculateSequenceWindow.R
@@ -1,4 +1,18 @@
-#' @export
+#'  Append a window column to a given data frame
+#'
+#'  This function silently drops any sites that do not have enough neighbouring
+#'  amino acids to make a complete window.
+#'
+#'  @param  dataframe   data.frame with a uniprot column, and another numeric column with site
+#'  @param  sitecol     Name of the column containing the site information
+#'  @param  window      Half-width of window (\code{window} of \code{3} results in 7-aa windows)
+#'  @examples
+#'    df <- data.frame(uniprot='q14118',site=c(30,40,50))
+#'    # 11-aa windows
+#'    calculateSequenceWindow(df,'site',5)
+#'    # 5-aa windows
+#'    calculateSequenceWindow(df,'site',2)
+#'  @export
 calculateSequenceWindow <- function(dataframe,sitecol,window) {
   with_seqs <- merge(dataframe,gator.UniProtData,by='uniprot')
   with_seqs[[sitecol]] <- as.numeric(with_seqs[[sitecol]])
