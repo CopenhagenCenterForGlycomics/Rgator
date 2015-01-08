b91ae0a6f547481ec216489fe188a37f262feefc
Hiren Joshi <hirenj@gmail.com>
2015-01-08 15:21:42 +0100

    Method to do the string community finding

--- R/string.R
+++ R/string.R
@@ -34,13 +34,34 @@ getStringNetwork <- function(organism=9606,accessions=c(),get.neighbours=F,thres
   getBiocLiteLib('STRINGdb')
   require('igraph')
   string_db <- STRINGdb::STRINGdb$new(species=organism,version="9_05",score_threshold=threshold)
-  all_string_proteins <- string_db$get_proteins()$protein_external_id
+
+  id_mappings <- prepare_string_ids(string_db,organism,accessions,get.neighbours)
+
+  subnetwork <- intergraph::asDF(string_db$get_subnetwork(id_mappings$STRING_id))
+
+  subnetwork$vertexes$name <- plyr::mapvalues(subnetwork$vertexes$name,id_mappings$STRING_id,tolower(id_mappings$uniprot),warn_missing=F)
+  subnetwork$edges$V1 <- plyr::mapvalues(subnetwork$edges$V1, subnetwork$vertexes$intergraph_id, subnetwork$vertexes$name,warn_missing=F)
+  subnetwork$edges$V2 <- plyr::mapvalues(subnetwork$edges$V2, subnetwork$vertexes$intergraph_id, subnetwork$vertexes$name,warn_missing=F)
+  net <- network::network(subset(subnetwork$edges,select=c(1,2),!is.na(V1) & !is.na(V2) ),directed=F)
+
+  network::set.edge.attribute(net,attrname="combined",value=subnetwork$edges$combined_score)
+  network::set.edge.attribute(net,attrname="experimental",value=subnetwork$edges$experimental)
+  network::set.edge.attribute(net,attrname="type",value=rep(NA,nrow(subnetwork$edges)))
+
+  # Sure, a "rep" call here will do the job, but we might want to change the class in the future
+  network::set.vertex.attribute(net,attrname='class',value=as.character(sapply(network::network.vertex.names(net),function(id) { "Protein" })))
+  network::set.vertex.attribute(net,attrname='uniprot',value=as.character(sapply(network::network.vertex.names(net),function(id) { toupper(id) })))
+  return (net)
+}
+
+prepare_string_ids <- function(string_db,organism=9606,accessions=c(),get.neighbours=F) {
   organisms <- list('9606'='org.Hs.eg.db','10090'='org.Mm.eg.db','10116'='org.Rn.eg.db','7227'='org.Dm.eg.db','4932'='org.Sc.sgd.db')
   dbname<-organisms[[as.character(organism)]]
   getBiocLiteLib(dbname)
   id_mappings <- AnnotationDbi::select(get(dbname),columns=c('UNIPROT','ENSEMBLPROT'),keys=toupper(accessions),keytype='UNIPROT')
   id_mappings$ENSEMBLPROT <- sapply ( id_mappings$ENSEMBLPROT, function(x) { paste(organism,'.',x,sep='') })
   names(id_mappings) <- c('uniprot','STRING_id')
+  all_string_proteins <- string_db$get_proteins()$protein_external_id
 
   # The id_mappings frame determine which IDs we will be retrieving from the String database.
 
@@ -60,20 +81,29 @@ getStringNetwork <- function(organism=9606,accessions=c(),get.neighbours=F,thres
     neighbour_mappings <- subset(neighbour_mappings,select=c('uniprot','STRING_id'))
     id_mappings <- rbind(id_mappings,neighbour_mappings)
   }
+  return(id_mappings)
+}
 
-  subnetwork <- intergraph::asDF(string_db$get_subnetwork(id_mappings$STRING_id))
+getStringClusters <- function(organism=9606,accessions=c(),get.neighbours=F,threshold=700,total.clusters=NA,algorithm=NA,...) {
+  getBiocLiteLib('STRINGdb')
+  require('igraph')
+  string_db <- STRINGdb::STRINGdb$new(species=organism,version="9_1",score_threshold=threshold)
 
-  subnetwork$vertexes$name <- plyr::mapvalues(subnetwork$vertexes$name,id_mappings$STRING_id,tolower(id_mappings$uniprot),warn_missing=F)
-  subnetwork$edges$V1 <- plyr::mapvalues(subnetwork$edges$V1, subnetwork$vertexes$intergraph_id, subnetwork$vertexes$name,warn_missing=F)
-  subnetwork$edges$V2 <- plyr::mapvalues(subnetwork$edges$V2, subnetwork$vertexes$intergraph_id, subnetwork$vertexes$name,warn_missing=F)
-  net <- network::network(subset(subnetwork$edges,select=c(1,2),!is.na(V1) & !is.na(V2) ),directed=F)
+  id_mappings <- prepare_string_ids(string_db,organism,accessions,get.neighbours)
 
-  network::set.edge.attribute(net,attrname="combined",value=subnetwork$edges$combined_score)
-  network::set.edge.attribute(net,attrname="experimental",value=subnetwork$edges$experimental)
-  network::set.edge.attribute(net,attrname="type",value=rep(NA,nrow(subnetwork$edges)))
-
-  # Sure, a "rep" call here will do the job, but we might want to change the class in the future
-  network::set.vertex.attribute(net,attrname='class',value=as.character(sapply(network::network.vertex.names(net),function(id) { "Protein" })))
-  network::set.vertex.attribute(net,attrname='uniprot',value=as.character(sapply(network::network.vertex.names(net),function(id) { toupper(id) })))
-  return (net)
+  if (is.na(algorithm)) {
+    algorithm <- igraph::fastgreedy.community
+  }
+  community.data <- algorithm( string_db$get_subnetwork(id_mappings$STRING_id), ... )
+  if (! is.na(total.clusters) && igraph::is.hierarchical(community.data) ) {
+    memb = cutat(community.data,total.clusters)
+  } else {
+    memb = membership(community.data)
+  }
+  clusters = NULL
+  for (i in 1:max(memb)) {
+      clusters[[i]] = names(membership(community.data)[membership(community.data) == i])
+  }
+  clusters <- lapply(clusters, function(clust) { plyr::mapvalues(clust,id_mappings$STRING_id,tolower(id_mappings$uniprot),warn_missing=F) } )
+  return (clusters)
 }
