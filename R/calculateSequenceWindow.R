70d311e45f1a498575cd8dd3f5ecd47a53fd510e
Hiren Joshi <hirenj@gmail.com>
2016-04-18 09:01:58 +0200

    Fix for doco not exporting name

--- R/calculateSequenceWindow.R
+++ R/calculateSequenceWindow.R
@@ -1,18 +1,18 @@
-#'  Append a window column to a given data frame
+#' Append a window column to a given data frame
 #'
-#'  This function silently drops any sites that do not have enough neighbouring
-#'  amino acids to make a complete window.
+#' This function silently drops any sites that do not have enough neighbouring
+#' amino acids to make a complete window.
 #'
-#'  @param  dataframe   data.frame with a uniprot column, and another numeric column with site
-#'  @param  sitecol     Name of the column containing the site information
-#'  @param  window      Half-width of window (\code{window} of \code{3} results in 7-aa windows)
-#'  @examples
+#' @param  dataframe   data.frame with a uniprot column, and another numeric column with site
+#' @param  sitecol     Name of the column containing the site information
+#' @param  window      Half-width of window (\code{window} of \code{3} results in 7-aa windows)
+#' @examples
 #'    df <- data.frame(uniprot='q14118',site=c(30,40,50))
 #'    # 11-aa windows
 #'    calculateSequenceWindow(df,'site',5)
 #'    # 5-aa windows
 #'    calculateSequenceWindow(df,'site',2)
-#'  @export
+#' @export
 calculateSequenceWindow <- function(dataframe,sitecol,window) {
   with_seqs <- unique(merge(dataframe,getUniprotSequences(unique(dataframe$uniprot)),by='uniprot'))
   with_seqs[[sitecol]] <- as.numeric(with_seqs[[sitecol]])
